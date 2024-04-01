dc29427e3451a78a9d5ef5c3c3097f946d3dd024
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-04-01 15:41:42 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,7 +1,11 @@
 <?php
 
+// definiu a pasta de destino
 $pastaDestino = "uploads/";
+
+// pegamos o nome do aquivo
 $nomeArquivo = $_FILES['fileToUpload']['name'];
+
 var_dump($_FILES['fileToUpload']['name']);
 var_dump($_SERVER);
 file_exists($_SERVER[] . $pastaDestinpo . $nomeArquivo);
