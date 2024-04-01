904a3ddaa051f773208e0046b74b4ad9e8cffad7
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-04-01 15:36:32 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,5 +3,5 @@
 $pastaDestino = "uploads/";
 $nomeArquivo = $_FILES['fileToUpload']['name'];
 var_dump($_FILES['fileToUpload']['name']);
-
+var_dump($_SERVER);
 file_exists($_SERVER[] . $pastaDestinpo . $nomeArquivo);
