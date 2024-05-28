be373afb4d552e47f605a2d22afa9f52927e1031
UrielMontanha <161373376+UrielMontanha@users.noreply.github.com>
2024-05-28 17:14:29 -0300

    great_upload

--- upload.php
+++ upload.php
@@ -6,4 +6,16 @@ var_dump($_FILES);
 
 var_dump($_FILES['fileToUpload']['size']);
 
+if ($_FILES['fileToUpload']['size'] > 2000000)
+{
+    echo "O tamanho do arquivo é maior que o limite permitido.";
+    die();
+}
+
+var_dump($_FILES['fileToUpload']['name']);
+var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_EXTENSION));
+var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_FILENAME));
+var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_BISENAME));
+var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_DIRNAME));
+
 ?>
