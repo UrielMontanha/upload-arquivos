01324fc4e10d2091af71f824bfd626f01575e46a
UrielMontanha <161373376+UrielMontanha@users.noreply.github.com>
2024-06-04 16:42:05 -0300

    .

--- upload.php
+++ upload.php
@@ -4,18 +4,18 @@
 $pastaDestino = "/uploads/";
 var_dump($_FILES);
 
-var_dump($_FILES['fileToUpload']['size']);
+var_dump($_FILES['arquivo']['size']);
 
-if ($_FILES['fileToUpload']['size'] > 2000000)
+if ($_FILES['arquivo']['size'] > 2000000)
 {
     echo "O tamanho do arquivo é maior que o limite permitido.";
-    die();
+    die();      
 }
 
-//var_dump($_FILES['fileToUpload']['name']);
-var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_EXTENSION));
+//var_dump($_FILES['arquivo']['name']);
+var_dump(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
 
-$extensao = pathinfo($_FILES['fileToUpload']['name'], PTHINFO_EXTENSION);
+$extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
 
 if ($extensao != "png" AND $extensao != "jpg" && $extensao != "jpeg" && $extensao != "gif" && $extensao != "jfif" && $extensao != "svg") {
     echo "O arquivo não é uma imagem! Apenas selecione arquivos com extenção png, jpg, jpeg, gif, jfif ou svg";
@@ -24,18 +24,28 @@ if ($extensao != "png" AND $extensao != "jpg" && $extensao != "jpeg" && $extensa
 
 
 
-if (getimagezise($_FILES['fileToUpload']['tmp_name']) === false) 
+if (getimagezise($_FILES['arquivo']['tmp_name']) === false)
 {
     echo "Problemas ao enviar a imagem. Tente novamente.";
     die();
 }
 
-//$FilesUpload 
+$fezUpload = move_uploaded_file($_FILES['arquivo']['tmp_name'], $pastaDestino . $_FILES['arquivo']['NAME']);
+
+IF($fezUpload == true)
+{
+    header("Location:index.php");
+} else {
+    echo "Erro ao mover arquivo.";
+}
 
 
 
-//var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_FILENAME));
-//var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_BISENAME));
-//var_dump(pathinfo($_FILES['fileToUpload']['name'], PTHINFO_DIRNAME));
+
+
+
+//var_dump(pathinfo($_FILES['arquivo']['name'], PTHINFO_FILENAME));
+//var_dump(pathinfo($_FILES['arquivo']['name'], PTHINFO_BISENAME));
+//var_dump(pathinfo($_FILES['arquivo']['name'], PTHINFO_DIRNAME));
 
 ?>
