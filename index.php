075ad3d157810c42fb9b1bc4470b48abeb254a99
UrielMontanha <161373376+UrielMontanha@users.noreply.github.com>
2024-06-07 15:03:13 -0300

    upload

--- index.php
+++ index.php
@@ -29,25 +29,45 @@ if ($resultado != false) {
     </form>
 
     <br><br>
-    <table border="12x"  style="border-color: #084d6e">
+    <table border="1">
         <thead>
             <tr>
                 <th> Nome arquivo </th>
-                <th cosplan="2"> Opções</th>
-            <tr>
+                <th colspan="2"> Opções</th>
+</tr>
         </thead>
         <tbody>
             <?php
             foreach ($arquivos as $arquivo) {
-                echo "<tr><td>" . $arquivo['nome_arquivo'] . "</td>";
-                echo "<td><a href='alterar.php?nome_arquivo=" . $arquivo['nome_arquivo'] . "'>Alterar</td>";
-                echo "<td><button>Excluir</button></td></tr>";
+                $arq = $arquivo['nome_arquivo'];
+                echo "<tr>"; //iniciar a linha
+                echo "<td> $arq </td>"; //1° coluna com o nome do aqruivo
+                echo "<td>"; //iniciar 2° coluna
+                echo "<a "; //abriu o link com a tag a
+                echo "href='alterar.php?nome_arquivo=$arq'>"; //
+                echo "Alterar"; //imprimiuo texto da tag a
+                echo "</a>"; //fechei a tag a (fechei o link)
+                echo "</td>"; //fechei a segunda coluna
+                echo "<td>"; //abri a 3° coluna
+                echo "<button "; //abrir o botão
+                echo "onclick="; //criou o atributo onclick
+                echo "'excluir(\"$arq \");'>"; //chamamos a função excluir
+                echo "Excluir"; //mostrar o texto do botão
+                echo "</button>"; //fechar o botão
+                echo "</td>"; //fechar a 3° coluna
+                echo "</tr>"; //fechar a linha
             }
             ?>
         </tbody>
         </table>
 
     </fieldset>
+
+    <script>
+        function excluir(nome_arquivo) {
+            confirm("Você tem certeza que deseja excluir o arquivo " + nome_arquivo);
+        }
+    </script>
 
 </body>
 </html>
